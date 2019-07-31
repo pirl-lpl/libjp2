2d482cf9cb6af720e26813c05c2166f004b106cd
guymac <guymac@gmail.com>
2019-07-31 12:27:30 -0700

    update include path for PVL

--- JP2_Metadata.cc
+++ JP2_Metadata.cc
@@ -25,7 +25,7 @@ Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 #include	"JP2_Exception.hh"
 
-#include	"PVL.hh"
+#include	"PVL/PVL.hh"
 using idaeim::PVL::Parameter;
 using idaeim::PVL::Aggregate;
 using idaeim::PVL::Assignment;
