1fc08171e5350563ac72b9310da8fd70f91bb9c8
GuyMac <guymac@gmail.com>
2024-11-05 14:01:34 -0700

    change Kakadu_Dir to KAKADU_DIR

--- FindKakadu.cmake
+++ FindKakadu.cmake
@@ -1,8 +1,8 @@
 set(KAKADU_VERSION 84)
 
-set(kdu_lib_path "${Kakadu_Dir}/lib/Linux-x86-64-gcc/" "${Kakadu_Dir}/lib/Mac-arm-64-gcc/" "${Kakadu_Dir}/lib/Mac-x86-64-gcc/" "${Kakadu_Dir}/../lib_x64/" "${Kakadu_Dir}/../lib_x86/")
+set(kdu_lib_path "${KAKADU_DIR}/lib/Linux-x86-64-gcc/" "${KAKADU_DIR}/lib/Mac-arm-64-gcc/" "${KAKADU_DIR}/lib/Mac-x86-64-gcc/" "${KAKADU_DIR}/../lib_x64/" "${KAKADU_DIR}/../lib_x86/")
 
-set(kdu_inc_path "${Kakadu_Dir}/managed/all_includes/")
+set(kdu_inc_path "${KAKADU_DIR}/managed/all_includes/")
 
 #message("Looking for Kakadu includes in ${kdu_inc_path}")
 
